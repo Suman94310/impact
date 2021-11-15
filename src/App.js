cf4906bac8a2c0e724b05ac296c6c419dc9a599f
Suman Saurav <suman@HP-Notebook-b8b9adbb.localdomain>
2021-11-16 03:12:48 +0530

    designed

--- src/App.js
+++ src/App.js
@@ -38,12 +38,14 @@ function App() {
     card : {
       display: 'flex',
       flexDirection: 'column',
-      width: '18rem',
-      height: '18rem',
-      maxWidth: '18rem',
-      maxHeight: '18rem',
-      border: '1px solid black',
-      borderRadius: '5px',
+      minWidth: `${tab[1]===":"?'12rem':'12rem'}`,
+      minHeight: `${tab[1]===":"?'17rem':'14rem'}`,
+      maxWidth: `${tab[1]===":"?'22rem':'12rem'}`,
+      maxHeight: `${tab[1]===":"?'27rem':'14rem'}`,
+      backgroundColor: '#fff',
+      borderRadius: '7px',
+      overflow: 'hidden',
+      boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.2)',
     },
     cardImage: {
       width: '100%',
@@ -58,16 +60,18 @@ function App() {
       flexDirection: 'row',
       justifyContent: 'space-between',
       alignItems: 'center',
-      width: '100%',
+      // width: '100%',
       height: '3.5em',
-      backgroundColor: '#152536',
-      marginBottom: '1em',
+      backgroundColor: '#252c35',
+      marginBottom: '2em',
+      padding: '0 5vw',
     },
     navbarItem: {
       textDecoration: 'none',
       color: '#fff',
-      fontSize: '1.2em',
-      backgroundColor: '#152536',
+      fontSize: '1.1em',
+      marginLeft: '2em',
+      fontWeight: '300',
       '&:hover': {
         backgroundColor: '#fff',
         color: '#152536',
@@ -78,20 +82,31 @@ function App() {
       color: '#152536',
     },
     search: {
-      width: '100%',
-      height: '3em',
-      border: '1px solid #152536',
-      borderRadius: '5px',
-      padding: '0.5em',
-      margin: '0.5em',
-      fontSize: '1.2em',
-      fontWeight: 'bold',
+      padding: '0.2em',
+      fontSize: '1em',
       backgroundColor: '#fff',
+      border: '1px solid #152536',
       color: '#152536',
       '&:focus': {
         outline: 'none',
       },
     },
+    sort: {
+      padding: '1em',
+      fontSize: '1em',
+      backgroundColor: '#239e2f',
+      width: "50%",
+      border: 'none',
+      color: '#fff'
+    },
+    reject: {
+      padding: '1em',
+      fontSize: '1em',
+      backgroundColor: '#bd0924',
+      width: "50%",
+      border: 'none',
+      color: '#fff'
+    }
   }
 
   const sortlistCandidate = (candidate) => {
@@ -103,6 +118,8 @@ function App() {
 
     localStorage.setItem('rejected', JSON.stringify(rejected.filter(item => item.id !== candidate.id)));
     setRejected(rejected.filter(item => item !== candidate));
+    // redirect to home
+    window.location = window.location.protocol + '//' + window.location.host+ '/';
   }
 
   const rejectCandidate = (candidate) => {
@@ -114,6 +131,8 @@ function App() {
 
     localStorage.setItem('sortlist', JSON.stringify(sortlist.filter(item => item.id !== candidate.id)));
     setSortlist(sortlist.filter(item => item !== candidate));
+    // redirect to home
+    window.location = window.location.protocol + '//' + window.location.host+ '/';
   }
 
   const searchCandidate = (candidate) => {
@@ -130,8 +149,8 @@ function App() {
           <img src={canditate.Image} className="card-img-top" width="100%" alt="..." style={styles.cardImage}/>
           <div className="card-body" style={styles.cardBody}>
             <h5 className="card-title">{canditate.name}</h5>
-            {buttons &&<button className="btn btn-primary" onClick={() => sortlistCandidate(canditate)}>sortlist</button>}
-            {buttons && <button className="btn btn-danger" onClick={() => rejectCandidate(canditate)}>Reject</button>}
+            {buttons &&<button className="btn btn-primary" onClick={(e) => {e.preventDefault();sortlistCandidate(canditate)}} style={styles.sort}>Sortlist</button>}
+            {buttons && <button className="btn btn-danger" onClick={(e) => {e.preventDefault();rejectCandidate(canditate)}} style={styles.reject}>Reject</button>}
           </div>
         </div>
       </a>
@@ -155,12 +174,37 @@ function App() {
 
   return (
     <div className="App" style={styles.outerContainer}>
+      <style>
+        {`
+          .App {
+            background-color: #f5f5f5;
+          }
+          body {
+            background-color: #f5f5f5;
+            height: 100%;
+          }
+          a {
+            text-decoration: none;
+            font-color: inherit;
+          }
+          .card {
+            font-size: 1em;
+            color: #616161;
+          }
+          button:hover {
+            cursor: pointer;
+          }
+        `}
+      </style>
       <div className="navbar" style={styles.navbar}>
+        <a href="/" style={styles.navbarItem}>Home</a>
         <div className="search">
-          <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)}/>
+          <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} style={styles.search}/>
         </div>
-        <a href="/rejected" style={styles.navbarItem}>Rejected</a>
-        <a href="/sortlist" style={styles.navbarItem}>Sortlist</a>
+        <div>
+          <a href="/rejected" style={styles.navbarItem}>Rejected</a>
+          <a href="/sortlist" style={styles.navbarItem}>Sortlist</a>
+        </div>
       </div>
       <div className="container" style={styles.container}>
         {tab==="/" && createCard(searchlist, false)}
